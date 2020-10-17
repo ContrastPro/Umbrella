a69544f18b1a4a9c457be2d0a79b85efb93f3964
ContrastPro <leojugaschvili@gmail.com>
2020-10-17 21:16:47 +0300

    Design fix #3

--- lib/global/colors.dart
+++ lib/global/colors.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
 
-/// Light Mode
+///App Themes
+
+// Light Theme
+ThemeData lightTheme = ThemeData(
+  brightness: Brightness.light,
+  accentColor: Colors.blueGrey,
+);
+
+// Dark Theme
+ThemeData darkTheme = ThemeData(
+  brightness: Brightness.dark,
+);
+
+///Colors
+
+// Light Mode
 const cl_background = const Color(0xffe0e5ec);
 const cl_darkShadow = const Color(0xffbec3c9);
-const cl_lightShadow = const Color(0xffffffff);
+const cl_lightShadow = const Color(0xfff5f5f5);
 const tl_primary = const Color(0xff323232);
 
-/// Dark Mode
+// Dark Mode
 const cd_background = const Color(0xff292d32);
 const cd_darkShadow = const Color(0xff23262b);
-const cd_lightShadow = const Color(0xff2f343e);
+const cd_lightShadow = const Color(0xff2f343a);
 const td_primary = const Color(0xffffffff);
+
+///Box Shadows for all boxes
+
+BoxShadow setBoxShadowDark(bool darkMode) {
+  return BoxShadow(
+      color: darkMode != true ? cl_darkShadow : cd_darkShadow,
+      offset: Offset(4.0, 4.0),
+      blurRadius: 15.0,
+      spreadRadius: 1.0);
+}
+
+BoxShadow setBoxShadowLight(bool darkMode) {
+  return BoxShadow(
+      color: darkMode != true ? cl_lightShadow : cd_lightShadow,
+      offset: Offset(-4.0, -4.0),
+      blurRadius: 15.0,
+      spreadRadius: 1.0);
+}
